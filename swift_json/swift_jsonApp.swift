e243b358bed2aaea6fa11d4a7b57b90e90b8ea48
shunooida <gpu.shun@gmail.com>
2022-05-20 20:09:54 +0900

    Initial Commit

---
+++ swift_json/swift_jsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_jsonApp.swift
+//  swift_json
+//
+//  Created by 大井田駿 on 2022/05/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_jsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
